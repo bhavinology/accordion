4ccb980d6448bdac985bf2d10b211da03abd6f03
Bhavin <bhavinpatel9624@gmail.com>
2024-03-28 16:34:23 +0530

    more efficient solution with 0 code duplication is implemented using switch case statement

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const a1 = document.querySelector("#a1");
 const q2 = document.querySelector("#q2");
 const a2 = document.querySelector("#a2");
 
-q1.addEventListener("click", function () {
-  console.log("q1");
-  a1.classList.toggle("hide");
-});
+q1.addEventListener("click", generalHandler);
+q2.addEventListener("click", generalHandler);
 
-q2.addEventListener("click", function () {
-  console.log("q2");
-  a2.classList.toggle("hide");
-});
+function generalHandler(e) {
+  switch (e.target.id) {
+    case "q1":
+      a1.classList.toggle("hide");
+      break;
+    case "q2":
+      a2.classList.toggle("hide");
+      break;
+    default:
+      console.log("No Code");
+  }
+}
