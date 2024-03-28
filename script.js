3e2ecd5b2e507338d98973dc41698e2a4679b7d1
Bhavin <bhavinpatel9624@gmail.com>
2024-03-28 16:41:00 +0530

    down arrow icon is positioned appropriately using position property and animated using transform through js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const q1 = document.querySelector("#q1");
 const a1 = document.querySelector("#a1");
 const q2 = document.querySelector("#q2");
 const a2 = document.querySelector("#a2");
+const icon = document.querySelectorAll(".fa-solid");
+console.log(icon);
 
 q1.addEventListener("click", generalHandler);
 q2.addEventListener("click", generalHandler);
@@ -10,9 +12,11 @@ function generalHandler(e) {
   switch (e.target.id) {
     case "q1":
       a1.classList.toggle("hide");
+      icon[0].classList.toggle("rotate");
       break;
     case "q2":
       a2.classList.toggle("hide");
+      icon[1].classList.toggle("rotate");
       break;
     default:
       console.log("No Code");
