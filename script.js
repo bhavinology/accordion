92044d543c0b058dfa127453c25218ff962dac60
Bhavin <bhavinpatel9624@gmail.com>
2024-03-28 16:29:10 +0530

    basic solution is implemented for multiple que/ans with lots of code duplication

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const q1 = document.querySelector("#q1");
 const a1 = document.querySelector("#a1");
+const q2 = document.querySelector("#q2");
+const a2 = document.querySelector("#a2");
 
 q1.addEventListener("click", function () {
+  console.log("q1");
   a1.classList.toggle("hide");
+});
+
+q2.addEventListener("click", function () {
+  console.log("q2");
+  a2.classList.toggle("hide");
 });
